f1b0588bfe215ca135635975addec06f50fa52c4
gaolei <470579032@qq.com>
2017-02-13 21:23:49 +0800

    关联第三方QQ

--- app/src/main/java/com/gl/newtitlegaolei/Fragment/FragmentMy.java
+++ app/src/main/java/com/gl/newtitlegaolei/Fragment/FragmentMy.java
@@ -1,13 +1,20 @@
 package com.gl.newtitlegaolei.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.gl.newtitlegaolei.R;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 
 /**
@@ -15,11 +22,80 @@ import com.gl.newtitlegaolei.R;
  * 时间：2017/2/9 10:54
  * 班级：1501A
  */
-public class FragmentMy extends Fragment{
+public class FragmentMy extends Fragment implements View.OnClickListener {
+
+    private ImageView dsf_qq;
+    private static String  gAppid="1105924500";
+    private Tencent ginstance;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_my,null);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        initKun();
+        initlistenter();
+    }
+    //点击事件
+    private void initlistenter() {
+        dsf_qq.setOnClickListener(this);
+    }
+    //找控件
+    private void initKun() {
+        ImageView dsf_phone=  (ImageView) getActivity().findViewById(R.id.DSF_phone);
+        ImageView dsf_weixin= (ImageView) getActivity().findViewById(R.id.DSF_weixin);
+        dsf_qq = (ImageView) getActivity().findViewById(R.id.DSF_qq);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.DSF_qq:
+                DSFQQ();
+                break;
+        }
+    }
+
+    private void DSFQQ() {
+        ginstance = Tencent.createInstance(gAppid,getContext());
+        dsf_qq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ginstance.login(FragmentMy.this,"all",loginlistener);
+            }
+        });
+    }
+    IUiListener loginlistener = new BaseUiListener(){
+
+    };
+
+    private class BaseUiListener implements IUiListener{
+
+
+        @Override
+        public void onComplete(Object o) {
+            Log.d("SDKQQAgentPref", "AuthorSwitch_SDK:"+ SystemClock.elapsedRealtime());
+        }
+
+        @Override
+        public void onError(UiError uiError) {
+            Log.i("gaolei",uiError.toString());
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Tencent.onActivityResultData(requestCode,resultCode,data,loginlistener);
     }
 }
